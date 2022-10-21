b8fe5244043d98c5a04686c4317d5d994295d1c8
Samuel Ximenes <samuel.ximenes.oliveira60@aluno.ifce.edu.br>
2022-10-21 00:03:10 -0300

    Created login and register flux with firebase, navbar created and routes working

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD-MX0CQJSdu6fICzGuIRiQuFsLqTmXRg4",
+    authDomain: "smart-infra.firebaseapp.com",
+    projectId: "smart-infra",
+    storageBucket: "smart-infra.appspot.com",
+    messagingSenderId: "388511120554",
+    appId: "1:388511120554:web:a0c1dc5d8fd1961b1fa4d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
